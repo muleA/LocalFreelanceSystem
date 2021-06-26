bc2e77e0c35338fcab6c4eb9dbf779005b4d57a2
Mulugeta Adamu <56386485+muleA@users.noreply.github.com>
2021-06-26 11:46:18 +0300

    landing page

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+</head>
+
+<body>
+<!--  navigation bar -->
+
+<nav class="navbar navbar-expand-lg fixed-top " style="background-color: whitesmoke!important;">
+  <a class="navbar-brand" href="#"><img src="Image\logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home | <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#how">how It works? |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">what do we offer? |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">about us |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">contact us |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">FAQ |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Policy</a>
+      </li>
+      
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0 mr-5">
+    <li class="nav-item"  style="list-style-type: none;">
+        <a class="nav-link" href="#">sign in </a>
+      </li>
+    <li class="nav-item dropdown" style="list-style-type: none;">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Join 
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">service provider</a>
+          <a class="dropdown-item" href="#">service seeker</a>
+        </div>
+      </li>
+    </form>
+  </div>
+</nav>
+<!--  -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="Image\coat.jpg" alt="First slide">
+     
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="Image\computer.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="Image\mug.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true" style="color: blue;"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!--  -->
+
+<!--  part 2 -->
+<div class="container fluid">
+
+<div class="row mt-5" >
+<div class="col-sm-6">
+<div class="jumbotron">
+<h1>are you service seeker?</h1>
+			<p>It's easy. Simply post a job you need completed and receive competitive bids from freelancers within minutes. Whatever your needs, there will be a freelancer to get it done: from web design, mobile app development, virtual assistants, product manufacturing, and graphic design (and a whole lot more). It is the simplest and safest way to get work done online.</p>
+			<p></p>
+			<a href="loginReg.php" class="btn btn-success btn-lg">Get Started</a>
+</div>
+</div>
+
+<div class="col-sm-6">
+<div class="jumbotron">
+<h1>Are you service provider?</h1>
+			<p>If you are an expert in any kind of computer related or online work, then do not hesitate to join our platform. It is easy to use and payment is secured. It is a great platform to those people who are skillful. So do not miss the chance to explore the job posts and make some money.</p>
+			<p></p>
+			<a href="loginReg.php" class="btn btn-primary btn-lg">Get Started</a>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<!--  -->
+
+<!--How it works-->
+<div style="background:#cce5ff">
+<div class="container text-center" style="padding:4%;" id="how">
+	<h1 class="card header2" style="background:#007BFF">How it works</h1>
+	<div class="row card" style="padding:30px 60px 30px 60px;margin:30px;">
+		<div class="col-lg-6">
+			<h3>Post Projects For Free</h3>
+			<p>It's always free to post your project. You’ll automatically begin to receive bids from our freelancers. 
+        Also, you can browse through the talent available on our site, and contact them by the contact information.
+        <img src="image/project.jpg"></p>
+		</div>
+<!-- 		<div class="col-lg-6">
+			<img src="image/project.jpg">
+		</div> -->
+	</div>
+	<div class="row card" style="padding:30px 60px 30px 60px;margin:30px;">
+		<div class="col-lg-6">
+			<h3>Deposit Money Safely</h3>
+			<p>We have a complete security to your valuable money. You have the rights to pay the deposited money after the project completed. We have a good refund policy to make sure of satisfaction of the project completed.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="image/depoite.jpg">
+		</div>
+	</div>
+	<div class="row card" style="padding:30px 60px 30px 60px;margin:30px;">
+		<div class="col-lg-6">
+			<h3>Feel Free To Talk</h3>
+			<p>It is easier to talk with the freelancers here. So before you hire any freelancer feel free to talk with them. Tell them what you need and get the project done in the shortest possible time.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="image/img03.jpg">
+		</div>
+	</div>
+	<div class="row card" style="padding:30px 60px 30px 60px;margin:30px;">
+		<div class="col-lg-6">
+			<h3>Build An Employer Profile</h3>
+			<p>If you have a lot of works to be done or run a small business that needs some freelancers in a daily basis, this is the perfect place for you. Build your employer profile today and start hiring.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="image/img04.jpg">
+		</div>
+	</div>
+</div>
+</div>
+<!--End How it works-->
+
+
+
+<!--FAQ-->
+<div class="container text-center" style="padding:4%;" id="faq">
+  <h1 class="card header2" style="background:#007BFF">FAQ</h1>
+  <div class="btn-group-vertical">
+  <button type="button" class="btn btn-default btn-lg" data-toggle="collapse" data-target="#demo" style="width:1062px"><h3>What is Freelance Marketplace?</h3></button>
+  <div id="demo" class="collapse">
+  	<div class="card" style="padding:20px 40px 20px 40px;">
+  		<h4>Freelance marketplace is an online outsourcing platform that puts employers and businesses in contact with a global network of freelancers. Any member can post a project, whether a short- or long-term job, and choose from skilled freelancers who offer bid proposals with rate for completing the work. It's a mutually beneficial arrangement. <br>Employers can have their pick of thousands of freelancers who have the exact skills needed to get the job done, without undertaking the expense and commitment of hiring full-time employees in person. <br>Freelancers can tap into a readily available source of constant part-time and full-time work opportunities from employers who are specifically seeking them out.</h4>
+  	</div>
+  </div>
+  <button type="button" class="btn btn-default btn-lg" data-toggle="collapse" data-target="#demo1" style="width:1062px"><h3>I am an Employer, how will this site work for me?</h3></button>
+  <div id="demo1" class="collapse">
+  	<div class="card" style="padding:20px 40px 20px 40px;">
+  		<h4>You can gain a competitive advantage over your competition by tapping into a skilled global workforce on demand. <br>If you are a small business and can't afford to hire a full-time staff, don't fret! The power of Freelancer is available for small to medium businesses! Whether it's a website that needs building, business cards or stationery that needs designing, a product that needs to be designed or manufactured, or research that needs to be done, this is the place for you! <br>Thousands of skilled workers are ready to start working right now! All you need to do is post a project!</h4>
+  	</div>
+  </div>
+  <button type="button" class="btn btn-default btn-lg" data-toggle="collapse" data-target="#demo2" style="width:1062px"><h3>I am a Freelancer, how will this site work for me?</h3></button>
+  <div id="demo2" class="collapse">
+  	<div class="card" style="padding:20px 40px 20px 40px;">
+  		<h4>With Freelancer, you can work at home and tap into a global network of businesses and projects across a huge range of industries - the ultimate opportunity in job flexibility! <br>Work on what you want, when you want and where you want to! The lifestyle of a freelancer is taking off. By working as a Freelancer online, you can greatly increase your client base and job throughput. <br>To start, all you need to do is sign up and start bidding. It's FREE!</h4>
+  	</div>
+  </div>
+  <button type="button" class="btn btn-default btn-lg" data-toggle="collapse" data-target="#demo3" style="width:1062px"><h3>Do I have to pay to register?</h3></button>
+  <div id="demo3" class="collapse">
+  	<div class="card" style="padding:20px 40px 20px 40px;">
+  		<h4>No. Freelance marketplace is absolutely free to register and explore the posted job offers, freelancers and employers.</h4>
+  	</div>
+  </div>
+  </div>
+</div>
+<!--End FAQ-->
+
+<!-- contact us -->
+
+<div class="text-center" style="padding:2%;color:#fff;margin-top:20px;">
+<hr>
+	<div class="row">
+	<div class="col-lg-3">
+			<h3>Social Contact</h3>
+			<p style="font-size:20px;color:#3B579D;"><i class="fab fa-facebook-square"> Facebook</i></p>
+			<p style="font-size:20px;color:#D34438;"><i class="fab fa-google-plus-square"> Google</i></p>
+			<p style="font-size:20px;color:#2CAAE1;"><i class="fab fa-twitter-square"> Twitter</i></p>
+			<p style="font-size:20px;color:#0274B3;"><i class="fab fa-linkedin"> Linkedin</i></p>
+		</div>
+  <div class="col-lg-3">
+	<div class="mapouter"><div class="gmap_canvas"><iframe width="300" height="200"
+   id="gmap_canvas" src="https://maps.google.com/maps?q=addis%20ababa%20university&t=&z=13&ie=
+   UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+   </iframe>
+		</div>
+	</div>
+	
+</div>
+
+<div class="col-lg-3 ml-5">
+<form>
+  <div class="form-group">
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Full Name">
+  </div>
+  <div class="form-group">
+    
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email">
+  </div>
+  <div class="form-group">
+    
+    <textarea  class="form-control" id="formGroupExampleInput2" placeholder="message"> </textarea>
+  </div>
+  <button class="btn-primary"> submit</button>
+</form>
+  
+		</div>
+  </div>
+</div>
+
+<!--  -->
+<!--Footer-->
+<hr style="color: #3B579D; border-style:solid;"> 
+<div class="text-center" style="padding:2%;color:blue;margin-top:1px;">
+footer
+</div>
+
+</body>
+
+
+</html>
